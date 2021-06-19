f9ce9d8cc66b13131bc28fe16b188de7fec86dd8
TenguTerror <vegasduard@gmail.com>
2021-06-19 14:41:47 -0600

    finished the rest of the program

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-const vehicles = ['Yamaha FZ-09', 'Trophy Truck', 'F1 Car', 'Nascar', 'Ducati Sport Classic'];
-const tracks = ['Laguna Seca', 'Nurburging', 'Baja 1000', 'Suzuka', 'Monza'];
-const postion = ['First', 'Seecond', 'Third', 'Dead Last'];
+const vehicles = [
+  "Yamaha FZ-09",
+  "Trophy Truck",
+  "F1 Car",
+  "Nascar",
+  "Ducati Sport Classic",
+];
+const tracks = ["Laguna Seca", "Nurburging", "Baja 1000", "Suzuka", "Monza"];
+const position = ["First", "Seecond", "Third", "Dead Last"];
 
-const randomIndex = arr => {
-    return arr[Math.floor(Math.random() * arr.length)]
+const randomIndex = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
 };
 
-// console.log(randomIndex(vehicles));
+const selectedVehicle = () => {
+  return randomIndex(vehicles);
+};
+
+const selectedTracks = () => {
+  return randomIndex(tracks);
+};
+
+const selectedPosition = () => {
+  return randomIndex(position);
+};
+
+const mixedMessage = () => {
+  console.log(
+    `I went to ${selectedTracks()} and drove a ${selectedVehicle()} and placed ${selectedPosition()}`
+  );
+};
+
+mixedMessage();
