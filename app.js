05fade1c72d46fe1023769c7a1d885f9f04b8e4c
TenguTerror <vegasduard@gmail.com>
2021-06-19 14:20:46 -0600

    made randomIndex work

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-const vehicles = ['Yamaha FZ-09', 'Trophy Truck', 'F1 Car', 'Nascar'];
-const tracks = ['Laguna Seca'];
+const vehicles = ['Yamaha FZ-09', 'Trophy Truck', 'F1 Car', 'Nascar', 'Ducati Sport Classic'];
+const tracks = ['Laguna Seca', 'Nurburging', 'Baja 1000', 'Suzuka', 'Monza'];
+const postion = ['First', 'Seecond', 'Third', 'Dead Last'];
+
+const randomIndex = arr => {
+    return arr[Math.floor(Math.random() * arr.length)]
+};
+
+// console.log(randomIndex(vehicles));
